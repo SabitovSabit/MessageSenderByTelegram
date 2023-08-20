abcfd0c5ca4007f7de1a38c79f52642609ed8077
SabitSabitov <SabitNs@code.edu.az>
2023-08-20 21:55:42 +0400

    is ready

---
+++ TelegramMessageSender/BotHandler.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TelegramMessageSender
+{
+    public class BotHandler
+    {
+        public static ITelegramBotClient bot = new TelegramBotClient("6216746998:AAGtiJd4pQTPDRbrOlsAy6vsc_2ru_7VuK4");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+
+            var message = update.Message;
+            await botClient.SendTextMessageAsync(message.Chat, "salam");
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+    }
+}
---
+++ TelegramMessageSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using TelegramMessageSender;
+
+
+
+
+string date = "2023-06-04T15:49:00Z";
+
+DateTime.TryParse(date, out DateTime parsed);
+
+Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:00Z"));
+
+Console.WriteLine(DateTime.Compare(parsed,DateTime.Now));
+
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = { }, 
+};
+BotHandler.bot.ReceiveAsync(
+    BotHandler.HandleUpdateAsync,
+    BotHandler.HandleErrorAsync,
+    receiverOptions,
+    cancellationToken
+);
+Console.ReadLine();
+
+
